d46b7201132873985d08399e527badc9f7d03e0c
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-12-11 01:00:33 +0700

    file main go

--- algoritma-iteratif/iteratif.go
+++ algoritma-iteratif/iteratif.go
@@ -1,1 +1,12 @@
 package algoritma_iteratif
+
+func iterativeSequential(data []string, target string) int {
+	var i int
+	for i = 0; i < len(data); i++ {
+		if data[i] == target {
+			println(data[i])
+			return i
+		}
+	}
+	return -1
+}
--- algoritma-rekursif/rekursif.go
+++ algoritma-rekursif/rekursif.go
@@ -1,1 +1,13 @@
 package algoritma_rekursif
+
+func recursiveSequential(data []string, target string, index int) int {
+	if index >= len(data) {
+		return -1 // Tidak ditemukan
+	}
+
+	if data[index] == target {
+		return index // Ditemukan
+	}
+
+	return recursiveSequential(data, target, index+1) // Pindah ke elemen berikutnya
+}
